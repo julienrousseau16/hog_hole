8a59ae10df6c3a220dc8d33a591782c30c34420c
Julien Rousseau <julienrousseau16@gmail.com>
2020-09-18 18:55:47 +0200

    dev branch created + a few graphic elements

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-      <h1>Le Trou du Cochon</h1>
+      <div className='App_container'>
+        <h1 className='App_title'>Le Trou du Cochon</h1>
+        <img className='App_logo' src={require('./pictures/hog.png')} alt='hog-logo' />
+        <img className='grass' src={require('./pictures/grass.png')} alt='grass' />
+      </div>
     </div>
   )
 }
