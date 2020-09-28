d1a77cb63c8d33a1be6519dc795299017bd4b7f4
Julien Rousseau <julienrousseau16@gmail.com>
2020-09-28 20:56:35 +0200

    input generation method changed

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,31 +2,16 @@ import React, { useState } from 'react'
 
 import Button from './Button'
 
-import PlayerInput from './PlayerInput'
-
 import './Settings.css'
 
 const Settings = ({ lang }) => {
 
   const [playersNumber, setPlayersNumber] = useState(2)
-  const [inputList, setInputList] = useState([])
   const [showInputs, setShowInputs] = useState(false)
-  // const [player1, setPlayer1] = useState('')
-  // const [player2, setPlayer2] = useState('')
-  // const [player3, setPlayer3] = useState('')
-  // const [player4, setPlayer4] = useState('')
-
-  const numSelect = e => {
-    setPlayersNumber(e.target.value)
-  }
-  const numValid = () => {
-    setShowInputs(true)
-    const tmp = []
-    for (let i = 0; i < playersNumber; i++) {
-      tmp.push({})
-    }
-    setInputList(tmp)
-  }
+  const [player1, setPlayer1] = useState('Porco Rosso')
+  const [player2, setPlayer2] = useState('Babe')
+  const [player3, setPlayer3] = useState('Porcinet')
+  const [player4, setPlayer4] = useState('Herta')
 
   return (
     <div className='Settings'>
@@ -45,21 +30,79 @@ const Settings = ({ lang }) => {
         </label>
         <div className='Number_selection'>
           <select
-            // onClick={() => setShowInputs(false)}
-            onChange={numSelect}>
+            onClick={() => setShowInputs(false)}
+            onChange={e => setPlayersNumber(e.target.value)}>
             <option value={2}>2</option>
             <option value={3}>3</option>
             <option value={4}>4</option>
           </select>
           <Button
-            onClick={numValid}
-            text={lang === 'fr' ? 'Valider' : 'OK'}
-            color='validate' />
+            onClick={() => setShowInputs(true)}
+            text={lang === 'fr' ? 'Valider' : 'OK'} />
         </div>
       </div>
-      <div className='inputs_container'>
-        {showInputs && inputList.map((item, index) => <PlayerInput key={index} i={index + 1} lang={lang} />)}
-      </div>
+
+      {showInputs &&
+        <div className='inputs_container'>
+          <label>
+            {lang === 'fr' ?
+              'Nom du Joueur 1'
+              : 'Name of Player 1'}
+          </label>
+          <input
+            required
+            onChange={e => setPlayer1(e.target.value)}
+            type='text'
+            value={player1}
+          />
+          <label>
+            {lang === 'fr' ?
+              'Nom du Joueur 2'
+              : 'Name of Player 2'}
+          </label>
+          <input
+            required
+            onChange={e => setPlayer2(e.target.value)}
+            type='text'
+            value={player2}
+          />
+          {playersNumber > 2 &&
+            <>
+              <label>
+                {lang === 'fr' ?
+                  'Nom du Joueur 3'
+                  : 'Name of Player 3'}
+              </label>
+              <input
+                required
+                onChange={e => setPlayer3(e.target.value)}
+                type='text'
+                value={player3}
+              />
+            </>
+          }
+          {playersNumber > 3 &&
+            <>
+              <label>
+                {lang === 'fr' ?
+                  'Nom du Joueur 4'
+                  : 'Name of Player 4'}
+              </label>
+              <input
+                required
+                onChange={e => setPlayer4(e.target.value)}
+                type='text'
+                value={player4}
+              />
+            </>
+          }
+          <Button
+            type='nav'
+            url='/gamesession'
+            text={lang === 'fr' ? 'C\'est parti !' : 'Here we go !'}
+            color='validate' />
+        </div>}
+
     </div>
   )
 }
