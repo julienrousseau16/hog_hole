91a58b225d0a9d6d4d9b08eb3f317f4a9bdb5d63
Julien Rousseau <julienrousseau16@gmail.com>
2020-09-29 18:41:19 +0200

    wip - game session kinda works... KINDA !!

--- src/components/GameSession.js
+++ src/components/GameSession.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import Button from './Button'
 
 import './GameSession.css'
 
@@ -9,13 +11,128 @@ const GameSession = ({ lang }) => {
   const [hole3, setHole3] = useState('empty')
   const [hole4, setHole4] = useState('empty')
   const [hole5, setHole5] = useState('empty')
+  const [eggsP1, setEggsP1] = useState(6)
+  const [eggsP2, setEggsP2] = useState(7)
+  const [p1Turn, setP1Turn] = useState(true)
+  const [dice, setDice] = useState()
 
-  const [eggsP1, setEggP1] = useState(6)
-  const [eggsP2, setEggP2] = useState(6)
-
-  const toggle = () => {
-    hole1 === 'empty' ? setHole1('full') : setHole1('empty')
+  const diceRoll = () => {
+    const random = Math.floor(Math.random() * (7 - 1) + 1)
+    setDice(random)
+    setP1Turn(!p1Turn)
   }
+
+  const diceResult = () => {
+    if (p1Turn) {
+      switch (dice) {
+        case 1:
+          if (hole1 === 'empty') {
+            setHole1('full')
+            setEggsP2(eggsP2 - 1)
+          } else {
+            setEggsP1(eggsP1 + 1)
+            setHole1('empty')
+          }
+          break;
+        case 2:
+          if (hole2 === 'empty') {
+            setHole2('full')
+            setEggsP2(eggsP2 - 1)
+          } else {
+            setEggsP1(eggsP1 + 1)
+            setHole2('empty')
+          }
+          break;
+        case 3:
+          if (hole3 === 'empty') {
+            setHole3('full')
+            setEggsP2(eggsP2 - 1)
+          } else {
+            setEggsP1(eggsP1 + 1)
+            setHole3('empty')
+          }
+          break;
+        case 4:
+          if (hole4 === 'empty') {
+            setHole4('full')
+            setEggsP2(eggsP2 - 1)
+          } else {
+            setEggsP1(eggsP1 + 1)
+            setHole4('empty')
+          }
+          break;
+        case 5:
+          if (hole5 === 'empty') {
+            setHole5('full')
+            setEggsP2(eggsP2 - 1)
+          } else {
+            setEggsP1(eggsP1 + 1)
+            setHole5('empty')
+          }
+          break;
+        default:
+          setEggsP2(eggsP2 - 1)
+          console.log('trou du lapin')
+          break;
+      }
+    } else {
+      switch (dice) {
+        case 1:
+          if (hole1 === 'empty') {
+            setHole1('full')
+            setEggsP1(eggsP1 - 1)
+          } else {
+            setEggsP2(eggsP2 + 1)
+            setHole1('empty')
+          }
+          break;
+        case 2:
+          if (hole2 === 'empty') {
+            setHole2('full')
+            setEggsP1(eggsP1 - 1)
+          } else {
+            setEggsP2(eggsP2 + 1)
+            setHole2('empty')
+          }
+          break;
+        case 3:
+          if (hole3 === 'empty') {
+            setHole3('full')
+            setEggsP1(eggsP1 - 1)
+          } else {
+            setEggsP2(eggsP2 + 1)
+            setHole3('empty')
+          }
+          break;
+        case 4:
+          if (hole4 === 'empty') {
+            setHole4('full')
+            setEggsP1(eggsP1 - 1)
+          } else {
+            setEggsP2(eggsP2 + 1)
+            setHole4('empty')
+          }
+          break;
+        case 5:
+          if (hole5 === 'empty') {
+            setHole5('full')
+            setEggsP1(eggsP1 - 1)
+          } else {
+            setEggsP2(eggsP2 + 1)
+            setHole5('empty')
+          }
+          break;
+        default:
+          setEggsP1(eggsP1 - 1)
+          console.log('trou du lapin')
+          break;
+      }
+    }
+  }
+
+  useEffect(() => {
+    diceResult()
+  }, [dice])
 
   return (
     <div className='GameSession'>
@@ -53,19 +170,25 @@ const GameSession = ({ lang }) => {
           alt='hole' />
       </div>
       <ul className='players_container'>
-        <li className='each_player'>
+        <li className={p1Turn ? 'currentPlayer' : 'waitingPlayer'}>
           <h3>Bugs Buddy</h3>
           <img src={require('../pictures/egg.png')} alt='egg' />
           <p>x {eggsP1}</p>
         </li>
-        <li className='each_player'>
+        <li className={!p1Turn ? 'currentPlayer' : 'waitingPlayer'}>
           <h3>Rabbid Jacob</h3>
           <img src={require('../pictures/egg.png')} alt='egg' />
           <p>x {eggsP2}</p>
         </li>
       </ul>
 
-      <button onClick={toggle}>Remplir 1</button>
+      <Button
+        onClick={diceRoll}
+        text={lang === 'fr' ?
+          'Lancer le dé'
+          : 'Roll the dice'}
+        color='blue' />
+      <p>{dice}</p>
     </div>
   )
 }
